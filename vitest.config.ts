5bc31182aa2bbe1a48ab2aff8b6c3e023680da4e
alan <alanngo673@gmail.com>
2024-02-24 10:34:16 -0600

    defineConfig imported from right source

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig({ 
     test: {
       globals: true,
       environment: 'jsdom',
