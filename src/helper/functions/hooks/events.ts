d9e09e749a728d8382b43a56e1b93da0272202aa
alan <alanngo673@gmail.com>
2025-09-16 14:32:55 -0500

    debug in console

--- src/helper/functions/hooks/events.ts
+++ src/helper/functions/hooks/events.ts
@@ -1,14 +1,19 @@
 import { useEffect } from "react"
 
-type Publish = <E>(name: string, payload: E) => boolean
-const publish = <E>(name: string, payload: E): boolean => window.dispatchEvent(new CustomEvent(name, { detail: payload }))
+type Publish = <E>(name: string, payload: E) => void
+const publish = <E>(name: string, payload: E): void => {
+    window.dispatchEvent(new CustomEvent(name, { detail: payload }))
+    console.log("publishing event", name, payload)
+}
 
 export const usePublisher = (): Publish => publish
 
 export const useSubscriber = <E>(name: string, callback: (e: E) => void): void => {
     useEffect(() => {
-        const handleEvent = (e: CustomEvent) =>{
-            callback(e.detail)
+        console.log("subscribed to event", name)
+
+        const handleEvent = (evt: CustomEvent) => {
+            callback(evt.detail)
         }
 
         window.addEventListener(name, handleEvent as EventListener)
